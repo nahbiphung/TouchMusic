b3ba32884a2b103ad2f21522f624e289342e3aca
Oai <phunggiaoai@gmail.com>
2019-04-04 16:26:14 +0700

    make fadeIn fadeOut Modal for Login v Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { AppComponent } from './app.component';
 // Modules
 import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
+import { SlideshowModule } from 'ng-simple-slideshow';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material Module
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 // Components
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -14,12 +22,9 @@ import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SlideshowModule } from 'ng-simple-slideshow';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+
+
+
 import { NavPlayerComponent } from './components/nav-player/nav-player.component';
 
 @NgModule({
--- src/app/components/nav-bar/nav-bar.js
+++ src/app/components/nav-bar/nav-bar.js
@@ -15,3 +15,33 @@ $(window).on('scroll',function(){
         $('header').removeClass('scroll-effect');
     }
 })
+
+function showRegisterForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        })
+
+        $('.modal-title').html('Register with');
+    })
+}
+function showLoginForm(){
+    $('.registerBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast',function(){
+            $('.login-footer').fadeIn('fast');
+        })
+
+        $('.modal-title').html('Login with');
+    })
+}
+
+function openLoginModal(){
+    showLoginForm();
+}
+function openRegisterModal(){
+    showRegisterForm();
+}
+
+
