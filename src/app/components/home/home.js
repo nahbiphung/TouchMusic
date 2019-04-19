05b7af9743ff03560c2e5a8778be0f9b75f1dfbd
Oai <phunggiaoai@gmail.com>
2019-04-19 16:30:06 +0700

    fix bug by adding ngx-slick-carousel

--- src/app/components/home/home.js
+++ src/app/components/home/home.js
@@ -1,24 +1,4 @@
 $(document).ready(function(){
-    $('.galery-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        speed: 800,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                speed: 800,
-                prevArrow: $('.prev'),
-                nextArrow: $('.next'),
-              }
-            }
-        ]
-      });
-
     $('.song-back-img').mouseover(function(){
         $('.song-playButton').css('display','block')
     });
