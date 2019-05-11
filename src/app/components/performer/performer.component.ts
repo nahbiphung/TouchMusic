226caf84356d190d0b09a15a96e8a74f64791470
PhuNguyen2506 <phu.nhn.15dh110172@outlook.com>
2019-05-12 00:23:32 +0700

    layout of profile

--- src/app/components/performer/performer.component.ts
+++ src/app/components/performer/performer.component.ts
@@ -13,18 +13,42 @@ export class PerformerComponent implements OnInit {
   private collectionData: AngularFirestoreCollection<any>;
   private documentData: AngularFirestoreDocument<any>;
   private data: Performer;
+  private songData: Song[];
+  private albumData: Album[];
   constructor(
     private db: AngularFirestore,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
     const getParams = this.route.snapshot.paramMap.get('id');
+    
     this.documentData = this.db.collection('Performer').doc(getParams);
     this.documentData.valueChanges().subscribe((res: Performer) => {
       if (res) {
         this.data = res;
       }
     });
+    this.documentData.snapshotChanges().subscribe((res: any) => {
+      if (res) {
+        this.collectionData = this.db.collection('Song', query =>
+          query.where('performerId', '==', res.payload.ref));
+        this.collectionData.valueChanges().subscribe((songRes: Song[]) => {
+          if (songRes) {
+            this.songData = songRes;
+            console.log(songRes);
+          }
+        });
+        this.collectionData = this.db.collection('Album', query =>
+          query.where('performerId', '==', res.payload.ref));
+        this.collectionData.valueChanges().subscribe((albumRes: Album[]) => {
+          if (albumRes) {
+            this.albumData = albumRes;
+            console.log(albumRes);
+          }
+        });
+      }
+    });
+
   }
 
 }
